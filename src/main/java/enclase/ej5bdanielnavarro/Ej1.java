05b0163395235078c3ffb0ae02822515a8dcaa2c
dnavmoy <dnavmoy119@g.educaand.es>
2023-12-18 10:15:03 +0100

    corregido pedir valor, con trycatch

--- src/main/java/enclase/ej5bdanielnavarro/Ej1.java
+++ src/main/java/enclase/ej5bdanielnavarro/Ej1.java
@@ -1,8 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package enclase.ej5bdanielnavarro;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -15,48 +15,57 @@ public class Ej1 {
 
     public static void main(String[] args) {
         Random aleatorio = new Random();
-        
-        double[] arrayDouble=new double[15];
+
+        double[] arrayDouble = new double[15];
         Arrays.fill(arrayDouble, 7.5);
-        
-       double[] copiaArrayDouble=Arrays.copyOf(arrayDouble, arrayDouble.length);
+
+        double[] copiaArrayDouble = Arrays.copyOf(arrayDouble, arrayDouble.length);
         System.out.println("mostrar arrayDouble");
         System.out.println(Arrays.toString(arrayDouble));
         System.out.println("Ahora la copia");
         System.out.println(Arrays.toString(copiaArrayDouble));
         System.out.println("son iguales?: " + Arrays.equals(arrayDouble, copiaArrayDouble));
-        
+
         System.out.println("cambiar un valor por 6.3");
-        arrayDouble[aleatorio.nextInt(arrayDouble.length)]=6.3;
-        
-         System.out.println("mostrar arrayDouble");
+        arrayDouble[aleatorio.nextInt(arrayDouble.length)] = 6.3;
+
+        System.out.println("mostrar arrayDouble");
         System.out.println(Arrays.toString(arrayDouble));
         System.out.println("Ahora la copia");
         System.out.println(Arrays.toString(copiaArrayDouble));
         System.out.println("son iguales?: " + Arrays.equals(arrayDouble, copiaArrayDouble));
-        
-        
+
         int[] arraInt = new int[100];
-        arraInt= ej2(arraInt);
+        arraInt = ej2(arraInt);
         System.out.println(Arrays.toString(arraInt));
         Arrays.sort(arraInt);
-        System.out.println("Array ordenado" + Arrays.toString(arraInt));
+        System.out.println("Array ordenado \n" + Arrays.toString(arraInt));
+      
         
-        Scanner preguntar = new Scanner(System.in);
-        System.out.println("introduce numero a buscar");
-        int numero = preguntar.nextInt();
-        preguntar.nextLine();
-        System.out.println("esta en la posicion "+Arrays.binarySearch(arraInt, numero));
+        int numero = preguntarNumero("introduce numero a buscar");
         
+        System.out.println("esta en la posicion: "
+                + Arrays.binarySearch(arraInt, numero));
+
     }
-    
-    
-    public static int[] ej2(int[] arrayInt){
+
+    public static int[] ej2(int[] arrayInt) {
         Random aleatorio = new Random();
-        for(int i=0;i<arrayInt.length;i++){
-            arrayInt[i]=aleatorio.nextInt(10,101);
+        for (int i = 0; i < arrayInt.length; i++) {
+            arrayInt[i] = aleatorio.nextInt(10, 101);
         }
-        
         return arrayInt;
+    }
+    public static int preguntarNumero(String texto){
+          Scanner preguntar = new Scanner(System.in);
+        int numero=0;
+        try {
+            System.out.println(texto);
+            numero = preguntar.nextInt();
+              preguntar.nextLine();
+        } catch (java.util.InputMismatchException IME) {
+            System.out.println("introduce valor valido");
+        }
+        return numero;
     }
 }
